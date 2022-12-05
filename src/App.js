405cf7b7a315fd3080d9300c8037b7d65e4ac39d
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-05 23:06:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import { Button } from "./components/Button/Button";
+import { Header } from "./components/Header/Header";
+import menuItems from "./data/menuItems.json";
 
 function App() {
   return (
     <div className="App">
+      <Header menuItems={menuItems} />
       <p>
         <Button text="Play" type="button"></Button>
       </p>
