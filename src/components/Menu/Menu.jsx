e2ce4c2debf019a1f673fd656ce71e5f96da9dde
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-08 21:00:14 +0200

    Update Menu.jsx

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,14 +2,36 @@ import { Component } from "react";
 import styles from "./Menu.module.scss";
 
 class Menu extends Component {
+  state = {
+    activeIndex: 0,
+  };
+
+  handleClick(e, index) {
+    e.preventDefault();
+    this.setState({
+      activeIndex: index,
+    });
+  }
+
   render() {
-    const elements = this.props.menuItems.map(({ id, link, text }) => (
-      <li key={id}>
-        <a href={link} className={styles.link}>
-          {text}
-        </a>
-      </li>
-    ));
+    const { menuItems } = this.props;
+    const { activeIndex } = this.state;
+
+    const elements = menuItems.map(({ id, link, text }, index) => {
+      const className =
+        activeIndex === index ? `${styles.link} ${styles.active}` : styles.link;
+      return (
+        <li key={id}>
+          <a
+            href={link}
+            className={className}
+            onClick={(e) => this.handleClick(e, index)}
+          >
+            {text}
+          </a>
+        </li>
+      );
+    });
     return <ul className={styles.menu}>{elements}</ul>;
   }
 }
