2c217b998473d7e1d81bb26d0bb854234c082465
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-08 21:00:11 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Button } from "./components/Button/Button";
 import { Header } from "./components/Header/Header";
 import Menu from "./components/Menu/Menu";
+import ToggleButton from "./components/ToggleButton/ToggleButton";
 import menuItems from "./data/menuItems.json";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <Header menuItems={menuItems} />
       <Menu menuItems={menuItems} />
+      <ToggleButton text="Click Me" />
       <p>
         <Button text="Play" type="button"></Button>
       </p>
