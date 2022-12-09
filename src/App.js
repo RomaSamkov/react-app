4c27a9b1709ea2b88a75f05f88e305a7f9e18af4
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-09 21:20:27 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Button } from "./components/Button/Button";
 import { Header } from "./components/Header/Header";
 import Menu from "./components/Menu/Menu";
 import ToggleButton from "./components/ToggleButton/ToggleButton";
+import Vote from "./components/Vote/Vote";
 import menuItems from "./data/menuItems.json";
 
 function App() {
@@ -10,12 +11,13 @@ function App() {
       <Header menuItems={menuItems} />
       <Menu menuItems={menuItems} />
       <ToggleButton text="Click Me" />
-      <p>
+      <Vote />
+      {/* <p>
         <Button text="Play" type="button"></Button>
       </p>
       <p>
         <Button text="Enter"></Button>
-      </p>
+      </p> */}
     </div>
   );
 }
