65cf0d9ce500f2598d49ec32d4d192c75ca5da45
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-10 21:20:43 +0200

    Update Vote.jsx

--- src/components/Vote/Vote.jsx
+++ src/components/Vote/Vote.jsx
@@ -2,7 +2,29 @@ import { Component } from "react";
 import styles from "./Vote.module.scss";
 
 class Vote extends Component {
+  state = {
+    JS: 0,
+    Phyton: 0,
+  };
+
+  countTotal() {
+    const { JS, Phyton } = this.state;
+    return JS + Phyton;
+  }
+
+  countPercentage(propertyName) {
+    const total = this.countTotal();
+    const value = this.state[propertyName];
+    const result = (value / total) * 100;
+    return result;
+  }
+
   render() {
+    const { JS, Phyton } = this.state;
+    const total = this.countTotal();
+    const jsPercentage = this.countPercentage("JS");
+    const phytonPercentage = this.countPercentage("Phyton");
+
     return (
       <div className={styles.wrapper}>
         <div className={styles.block}>
@@ -16,9 +38,21 @@ class Vote extends Component {
         </div>
         <div className={styles.block}>
           <h2 className={styles.heading}>Results:</h2>
-          <div className={styles.btnBlock}>For JS: 50%</div>
-          <div className={styles.btnBlock}>For Phyton: 50%</div>
-          <div className={styles.btnBlock}>All votes: 50 votes</div>
+          <div className={styles.btnBlock}>
+            For JS:
+            <ul>
+              <li>All:{JS}</li>
+              <li>In percentage:{jsPercentage}%</li>
+            </ul>
+          </div>
+          <div className={styles.btnBlock}>
+            For Phyton:
+            <ul>
+              <li>All:{Phyton}</li>
+              <li>In percentage:{phytonPercentage}%</li>
+            </ul>
+          </div>
+          <div className={styles.btnBlock}>All votes: {total} votes</div>
         </div>
       </div>
     );
