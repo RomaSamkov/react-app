8ad43972f4c1504caa22c857e34a7e220b23f33e
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-12 21:31:01 +0200

    Update Vote.jsx

--- src/components/Vote/Vote.jsx
+++ src/components/Vote/Vote.jsx
@@ -1,5 +1,8 @@
 import { Component } from "react";
+import Block from "./Block";
 import styles from "./Vote.module.scss";
+import VoteActions from "./VoteActions";
+import VoteResults from "./VoteResults";
 
 class Vote extends Component {
   state = {
@@ -14,46 +17,44 @@ class Vote extends Component {
 
   countPercentage(propertyName) {
     const total = this.countTotal();
+    if (!total) {
+      return 0;
+    }
     const value = this.state[propertyName];
     const result = (value / total) * 100;
-    return result;
+    return Number(result.toFixed(2));
   }
+
+  handleVote = (propertyName) => {
+    this.setState((prevState) => {
+      const value = prevState[propertyName];
+      return {
+        [propertyName]: value + 1,
+      };
+    });
+  };
 
   render() {
     const { JS, Phyton } = this.state;
     const total = this.countTotal();
     const jsPercentage = this.countPercentage("JS");
     const phytonPercentage = this.countPercentage("Phyton");
+    // const { handleVote } = this;
 
     return (
       <div className={styles.wrapper}>
-        <div className={styles.block}>
-          <h2 className={styles.heading}>Vote for:</h2>
-          <div className={styles.btnBlock}>
-            <button>JS</button>
-          </div>
-          <div className={styles.btnBlock}>
-            <button>Phyton</button>
-          </div>
-        </div>
-        <div className={styles.block}>
-          <h2 className={styles.heading}>Results:</h2>
-          <div className={styles.btnBlock}>
-            For JS:
-            <ul>
-              <li>All:{JS}</li>
-              <li>In percentage:{jsPercentage}%</li>
-            </ul>
-          </div>
-          <div className={styles.btnBlock}>
-            For Phyton:
-            <ul>
-              <li>All:{Phyton}</li>
-              <li>In percentage:{phytonPercentage}%</li>
-            </ul>
-          </div>
-          <div className={styles.btnBlock}>All votes: {total} votes</div>
-        </div>
+        <Block title="Votes for:">
+          <VoteActions handleVote={this.handleVote} />
+        </Block>
+        <Block title="Results:">
+          <VoteResults
+            total={total}
+            JS={JS}
+            Phyton={Phyton}
+            jsPercentage={jsPercentage}
+            phytonPercentage={phytonPercentage}
+          />
+        </Block>
       </div>
     );
   }
