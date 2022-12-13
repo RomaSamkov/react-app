afd7c4e463deaa9b5246332762e2a9bfedac45b8
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-13 21:05:57 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import { Button } from "./components/Button/Button";
-import { Header } from "./components/Header/Header";
-import Menu from "./components/Menu/Menu";
-import ToggleButton from "./components/ToggleButton/ToggleButton";
-import Vote from "./components/Vote/Vote";
-import menuItems from "./data/menuItems.json";
+import FormAddBook from "./components/FormAddBook/FormAddBook";
+// import { Header } from "./components/Header/Header";
+// import Menu from "./components/Menu/Menu";
+// import ToggleButton from "./components/ToggleButton/ToggleButton";
+// import Vote from "./components/Vote/Vote";
+// import menuItems from "./data/menuItems.json";
 
 function App() {
   return (
     <div className="App">
-      <Header menuItems={menuItems} />
-      <Menu menuItems={menuItems} />
-      <ToggleButton text="Click Me" />
-      <Vote />
-      {/* <p>
-        <Button text="Play" type="button"></Button>
-      </p>
-      <p>
-        <Button text="Enter"></Button>
-      </p> */}
+      {/* <Header menuItems={menuItems} /> */}
+      {/* <Menu menuItems={menuItems} /> */}
+      {/* <ToggleButton text="Click Me" /> */}
+      {/* <Vote /> */}
+      <FormAddBook />
     </div>
   );
 }
