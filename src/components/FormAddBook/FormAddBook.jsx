9b1b3284c740d43214b389e1c30ad35130338d6f
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-14 20:16:15 +0200

    Update FormAddBook.jsx

--- src/components/FormAddBook/FormAddBook.jsx
+++ src/components/FormAddBook/FormAddBook.jsx
@@ -7,18 +7,32 @@ class FormAddBook extends Component {
     author: "",
   };
 
-  handleChange = () => {
-    console.log("onChange");
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { onSubmit } = this.props;
+    onSubmit({ ...this.state });
+    this.setState({
+      title: "",
+      author: "",
+    });
   };
 
   render() {
-    const { handleChange } = this;
+    const { handleChange, handleSubmit } = this;
+    const { title, author } = this.state;
 
     return (
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className={styles.formGroup}>
           <label htmlFor="">Title</label>
           <input
+            value={title}
+            name="title"
             onChange={handleChange}
             className={styles.field}
             type="text"
@@ -28,6 +42,8 @@ class FormAddBook extends Component {
         <div className={styles.formGroup}>
           <label htmlFor="">Author</label>
           <input
+            value={author}
+            name="author"
             onChange={handleChange}
             className={styles.field}
             type="text"
