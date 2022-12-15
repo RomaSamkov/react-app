acd7406e09e8617b9bed86c4ea56da276a383923
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-15 22:39:58 +0200

    Create FormAddBook.jsx

---
+++ src/components/Books/FormAddBook/FormAddBook.jsx
@@ -0,0 +1,68 @@
+import { Component } from "react";
+import styles from "./FormAddBook.module.scss";
+import PropTypes from "prop-types";
+
+class FormAddBook extends Component {
+  state = {
+    title: "",
+    author: "",
+  };
+
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { onSubmit } = this.props;
+    onSubmit({ ...this.state });
+    this.setState({
+      title: "",
+      author: "",
+    });
+  };
+
+  render() {
+    const { handleChange, handleSubmit } = this;
+    const { title, author } = this.state;
+
+    return (
+      <form onSubmit={handleSubmit}>
+        <div className={styles.formGroup}>
+          <label htmlFor="">Title</label>
+          <input
+            value={title}
+            name="title"
+            onChange={handleChange}
+            className={styles.field}
+            type="text"
+            placeholder="Enter Title"
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="">Author</label>
+          <input
+            value={author}
+            name="author"
+            onChange={handleChange}
+            className={styles.field}
+            type="text"
+            placeholder="Enter Author"
+          />
+        </div>
+        <button className={styles.btn}>Add book</button>
+      </form>
+    );
+  }
+}
+
+export default FormAddBook;
+
+FormAddBook.defaultProps = {
+  onSubmit: () => {},
+};
+
+FormAddBook.propTypes = {
+  onSubmit: PropTypes.func,
+};
