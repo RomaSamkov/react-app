7dfbd368771c74819443c350bf7ba229a6e6be9c
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-22 23:07:29 +0200

    Create fields.js

---
+++ src/components/PostSearch/PostsSearchForm/fields.js
@@ -0,0 +1,11 @@
+const fields = {
+  search: {
+    label: "Search text",
+    type: "text",
+    name: "search",
+    placeholder: "Enter search text",
+    required: true,
+  },
+};
+
+export default fields;
