48d496aae767a007f26d469389d815da77884f00
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-22 23:07:39 +0200

    Create PostsSearchForm.jsx

---
+++ src/components/PostSearch/PostsSearchForm/PostsSearchForm.jsx
@@ -0,0 +1,53 @@
+import { Component } from "react";
+import TextField from "../../../servises/components/TextField/TextField";
+import fields from "./fields";
+import initialState from "./initialState";
+import PropTypes from "prop-types";
+import { nanoid } from "nanoid";
+import SubmitButton from "../../../servises/components/SubmitButton/SubmitButton";
+import styles from "./PostSearchForm.module.scss";
+
+class PostsSearchForm extends Component {
+  state = { ...initialState };
+  searchId = nanoid();
+
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const { onSubmit } = this.props;
+    onSubmit({ ...this.state });
+    this.setState({
+      title: "",
+      author: "",
+    });
+  };
+  render() {
+    const { search } = this.state;
+    const { handleChange, handleSubmit, searchId } = this;
+    return (
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <TextField
+          value={search}
+          id={searchId}
+          onChange={handleChange}
+          {...fields.search}
+        />
+        <SubmitButton text="Search" />
+      </form>
+    );
+  }
+}
+
+export default PostsSearchForm;
+
+PostsSearchForm.defaultProps = {
+  onSubmit: () => {},
+};
+
+PostsSearchForm.propTypes = {
+  onSubmit: PropTypes.func,
+};
