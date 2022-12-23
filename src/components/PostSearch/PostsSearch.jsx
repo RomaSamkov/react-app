72fb437377dba44781d753cfcaf845710184bee3
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-23 22:27:24 +0200

    add search

--- src/components/PostSearch/PostsSearch.jsx
+++ src/components/PostSearch/PostsSearch.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+import { searchPosts } from "../../servises/api/posts";
 import PostList from "../../servises/components/PostList/PostList";
 import styles from "./PostsSearch.module.scss";
 import PostsSearchForm from "./PostsSearchForm/PostsSearchForm";
@@ -8,12 +9,48 @@ class PostsSearch extends Component {
     items: [],
     loading: false,
     error: null,
+    search: "",
+  };
+
+  componentDidUpdate(_, prevState) {
+    const { search } = this.state;
+    if (prevState.search !== search) {
+      this.fetchPosts();
+    }
+  }
+
+  async fetchPosts() {
+    const { page } = this.state;
+    this.setState({
+      loading: true,
+    });
+
+    try {
+      const data = await searchPosts(page);
+      this.setState(({ items }) => ({
+        items: [...items, ...data],
+      }));
+    } catch (error) {
+      this.setState({
+        error,
+      });
+    } finally {
+      this.setState({ loading: false });
+    }
+  }
+
+  onSearch = ({ search }) => {
+    this.setState({
+      search,
+    });
   };
 
   render() {
+    const { onSearch } = this;
+
     return (
       <div>
-        <PostsSearchForm />
+        <PostsSearchForm onSubmit={onSearch} />
         <PostList />
       </div>
     );
