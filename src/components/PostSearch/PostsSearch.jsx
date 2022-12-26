2c5932af927790c075eb51ee5eda39eea798cfd4
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-26 22:06:32 +0200

    add page to searchPosts

--- src/components/PostSearch/PostsSearch.jsx
+++ src/components/PostSearch/PostsSearch.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
 import { searchPosts } from "../../servises/api/posts";
+import Modal from "../../servises/components/Modal/Modal";
 import PostList from "../../servises/components/PostList/PostList";
 import styles from "./PostsSearch.module.scss";
 import PostsSearchForm from "./PostsSearchForm/PostsSearchForm";
@@ -10,23 +11,25 @@ class PostsSearch extends Component {
     loading: false,
     error: null,
     search: "",
+    page: 1,
+    modalOpen: false,
   };
 
   componentDidUpdate(_, prevState) {
-    const { search } = this.state;
-    if (prevState.search !== search) {
+    const { search, page } = this.state;
+    if ((search && prevState.search !== search) || page !== prevState.page) {
       this.fetchPosts();
     }
   }
 
   async fetchPosts() {
-    const { page } = this.state;
+    const { search, page } = this.state;
     this.setState({
       loading: true,
     });
 
     try {
-      const data = await searchPosts(page);
+      const data = await searchPosts(search, page);
       this.setState(({ items }) => ({
         items: [...items, ...data],
       }));
@@ -45,13 +48,31 @@ class PostsSearch extends Component {
     });
   };
 
+  loadMore = () => {
+    this.setState(({ page }) => ({
+      page: page + 1,
+    }));
+  };
+
+  openModal = () => {
+    this.setState({
+      modalOpen: true,
+    });
+  };
+
   render() {
-    const { onSearch } = this;
+    const { onSearch, loadMore, openModal } = this;
+    const { items, loading, error, modalOpen } = this.state;
+    const isPosts = Boolean(items.length);
 
     return (
       <div>
+        {modalOpen && <Modal />}
         <PostsSearchForm onSubmit={onSearch} />
-        <PostList />
+        {isPosts && <PostList items={items} onClick={openModal} />}
+        {loading && <p>...Loading posts!!!</p>}
+        {error && <p>Failed Loading posts!</p>}
+        {isPosts && <button onClick={loadMore}>Load More</button>}
       </div>
     );
   }
