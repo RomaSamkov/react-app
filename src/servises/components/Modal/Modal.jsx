f758e1673f575f6628f828e267470bb3ae079dcf
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-27 23:05:55 +0200

    add componentDidMount and componentWillUnmount

--- src/servises/components/Modal/Modal.jsx
+++ src/servises/components/Modal/Modal.jsx
@@ -2,11 +2,29 @@ import { Component } from "react";
 import styles from "./Modal.module.scss";
 
 class Modal extends Component {
+  componentDidMount() {
+    document.addEventListener("keydown", this.closeModal);
+  }
+  closeModal = ({ target, currentTarget, code }) => {
+    if (target === currentTarget || code === "Escape") {
+      this.props.close();
+    }
+  };
+
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.closeModal);
+  }
+
   render() {
+    const { children } = this.props;
+    const { closeModal } = this;
     return (
-      <div className={styles.overlay}>
+      <div className={styles.overlay} onClick={closeModal}>
         <div className={styles.modal}>
-          <span className={styles.close}>X</span>
+          <span className={styles.close} onClick={closeModal}>
+            X
+          </span>
+          {children}
         </div>
       </div>
     );
