566bf067cb9b72ae6e8e613ee06fcc86cfdf34d1
RomaSamkov <Romansamkov.rs@gmail.com>
2022-12-30 19:22:53 +0200

    add holidays

--- src/components/PostSearch/PostsSearch.jsx
+++ src/components/PostSearch/PostsSearch.jsx
@@ -82,6 +82,7 @@ class PostsSearch extends Component {
 
     return (
       <div>
+        <h1 className={styles.holiday}>Happy Holiday !!!</h1>
         {modalOpen && (
           <Modal close={closeModal}>
             <h3 className={styles.postTitle}>{modalContent.title}</h3>
