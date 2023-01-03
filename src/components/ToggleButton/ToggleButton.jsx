e1e29361b7366a65eb04044c41065634937bd404
RomaSamkov <Romansamkov.rs@gmail.com>
2023-01-03 20:49:07 +0200

    add refactoring using hook

--- src/components/ToggleButton/ToggleButton.jsx
+++ src/components/ToggleButton/ToggleButton.jsx
@@ -1,31 +1,45 @@
-import { Component } from "react";
+import { useState } from "react";
 import styles from "./ToggleButton.module.scss";
 import PropTypes from "prop-types";
 
-class ToggleButton extends Component {
-  state = {
-    isActive: false,
-  };
+const ToggleButton = ({ text }) => {
+  const [isActive, setIsActive] = useState(false);
 
-  handleClick = () => {
-    this.setState((prevState) => {
-      return {
-        isActive: !prevState.isActive,
-      };
-    });
+  const handleClick = () => {
+    setIsActive((prevState) => !prevState);
   };
-  render() {
-    const { text } = this.props;
-    const { isActive } = this.state;
-    const { handleClick } = this;
-    const className = isActive ? `${styles.btn} ${styles.active}` : styles.btn;
-    return (
-      <button type="button" className={className} onClick={handleClick}>
-        {text}
-      </button>
-    );
-  }
-}
+  const className = isActive ? `${styles.btn} ${styles.active}` : styles.btn;
+  return (
+    <button type="button" className={className} onClick={handleClick}>
+      {text}
+    </button>
+  );
+};
+
+// class ToggleButton extends Component {
+//   state = {
+//     isActive: false,
+//   };
+
+//   handleClick = () => {
+//     this.setState((prevState) => {
+//       return {
+//         isActive: !prevState.isActive,
+//       };
+//     });
+//   };
+//   render() {
+//     const { text } = this.props;
+//     const { isActive } = this.state;
+//     const { handleClick } = this;
+//     const className = isActive ? `${styles.btn} ${styles.active}` : styles.btn;
+//     return (
+//       <button type="button" className={className} onClick={handleClick}>
+//         {text}
+//       </button>
+//     );
+//   }
+// }
 
 export default ToggleButton;
 
