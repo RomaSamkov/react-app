4b553f47cb05e110844f2b2efa1f5cc38358c77c
RomaSamkov <Romansamkov.rs@gmail.com>
2023-01-03 20:49:01 +0200

    add refactoring using hook

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,39 +1,64 @@
-import { Component } from "react";
+import { useState } from "react";
 import styles from "./Menu.module.scss";
 
-class Menu extends Component {
-  state = {
-    activeIndex: 0,
-  };
+const Menu = ({ menuItems }) => {
+  const [activeIndex, setActiveIndex] = useState(0);
 
-  handleClick(e, index) {
+  const handleClick = (e, index) => {
     e.preventDefault();
-    this.setState({
-      activeIndex: index,
-    });
-  }
+    setActiveIndex(index);
+  };
+  const elements = menuItems.map(({ id, link, text }, index) => {
+    const className =
+      activeIndex === index ? `${styles.link} ${styles.active}` : styles.link;
+    return (
+      <li key={id}>
+        <a
+          href={link}
+          className={className}
+          onClick={(e) => handleClick(e, index)}
+        >
+          {text}
+        </a>
+      </li>
+    );
+  });
+  return <ul className={styles.menu}>{elements}</ul>;
+};
 
-  render() {
-    const { menuItems } = this.props;
-    const { activeIndex } = this.state;
+// class Menu extends Component {
+//   state = {
+//     activeIndex: 0,
+//   };
 
-    const elements = menuItems.map(({ id, link, text }, index) => {
-      const className =
-        activeIndex === index ? `${styles.link} ${styles.active}` : styles.link;
-      return (
-        <li key={id}>
-          <a
-            href={link}
-            className={className}
-            onClick={(e) => this.handleClick(e, index)}
-          >
-            {text}
-          </a>
-        </li>
-      );
-    });
-    return <ul className={styles.menu}>{elements}</ul>;
-  }
-}
+//   handleClick(e, index) {
+//     e.preventDefault();
+//     this.setState({
+//       activeIndex: index,
+//     });
+//   }
+
+//   render() {
+//     const { menuItems } = this.props;
+//     const { activeIndex } = this.state;
+
+//     const elements = menuItems.map(({ id, link, text }, index) => {
+//       const className =
+//         activeIndex === index ? `${styles.link} ${styles.active}` : styles.link;
+//       return (
+//         <li key={id}>
+//           <a
+//             href={link}
+//             className={className}
+//             onClick={(e) => this.handleClick(e, index)}
+//           >
+//             {text}
+//           </a>
+//         </li>
+//       );
+//     });
+//     return <ul className={styles.menu}>{elements}</ul>;
+//   }
+// }
 
 export default Menu;
