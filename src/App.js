ae2e8fe722aa8bdbab78d1f6b956e212ad953fa8
RomaSamkov <Romansamkov.rs@gmail.com>
2023-01-03 20:48:54 +0200

    add refactoring using hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 // import Books from "./components/Books/Books";
 // import Posts from "./components/Posts/Posts";
-import PostsSearch from "./components/PostSearch/PostsSearch";
+// import PostsSearch from "./components/PostSearch/PostsSearch";
 
 // import { Header } from "./components/Header/Header";
-// import Menu from "./components/Menu/Menu";
-// import ToggleButton from "./components/ToggleButton/ToggleButton";
-// import Vote from "./components/Vote/Vote";
-// import menuItems from "./data/menuItems.json";
+import Menu from "./components/Menu/Menu";
+import ToggleButton from "./components/ToggleButton/ToggleButton";
+import Vote from "./components/Vote/Vote";
+import menuItems from "./data/menuItems.json";
 
 function App() {
   return (
     <div className="App">
       {/* <Header menuItems={menuItems} /> */}
-      {/* <Menu menuItems={menuItems} /> */}
-      {/* <ToggleButton text="Click Me" /> */}
-      {/* <Vote /> */}
+      <Menu menuItems={menuItems} />
+      <ToggleButton text="Click Me" />
+      <Vote />
       {/* <Books /> */}
       {/* <Posts /> */}
-      <PostsSearch />
+      {/* <PostsSearch /> */}
     </div>
   );
 }
