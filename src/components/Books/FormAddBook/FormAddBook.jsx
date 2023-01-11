d0aa2f34aa22a5f2bd2b7e1272d27bd361595c4b
RomaSamkov <Romansamkov.rs@gmail.com>
2023-01-11 22:48:33 +0200

    add refactoring on hooks in FormAddBook

--- src/components/Books/FormAddBook/FormAddBook.jsx
+++ src/components/Books/FormAddBook/FormAddBook.jsx
@@ -1,61 +1,112 @@
-import { Component } from "react";
+import { useState } from "react";
 import styles from "./FormAddBook.module.scss";
 import PropTypes from "prop-types";
 
-class FormAddBook extends Component {
-  state = {
+const FormAddBook = ({ onSubmit }) => {
+  const [state, setState] = useState({
     title: "",
     author: "",
-  };
+  });
 
-  handleChange = ({ target }) => {
+  const handleChange = ({ target }) => {
     const { value, name } = target;
-    this.setState({ [name]: value });
+    setState({ ...state, [name]: value });
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const { onSubmit } = this.props;
-    onSubmit({ ...this.state });
-    this.setState({
+    onSubmit({ ...state });
+    setState({
       title: "",
       author: "",
     });
   };
 
-  render() {
-    const { handleChange, handleSubmit } = this;
-    const { title, author } = this.state;
+  const { title, author } = state;
 
-    return (
-      <form onSubmit={handleSubmit}>
-        <div className={styles.formGroup}>
-          <label htmlFor="">Title</label>
-          <input
-            value={title}
-            name="title"
-            onChange={handleChange}
-            className={styles.field}
-            type="text"
-            placeholder="Enter Title"
-          />
-        </div>
-        <div className={styles.formGroup}>
-          <label htmlFor="">Author</label>
-          <input
-            value={author}
-            name="author"
-            onChange={handleChange}
-            className={styles.field}
-            type="text"
-            placeholder="Enter Author"
-          />
-        </div>
-        <button className={styles.btn}>Add book</button>
-      </form>
-    );
-  }
-}
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className={styles.formGroup}>
+        <label htmlFor="">Title</label>
+        <input
+          value={title}
+          name="title"
+          onChange={handleChange}
+          className={styles.field}
+          type="text"
+          placeholder="Enter Title"
+        />
+      </div>
+      <div className={styles.formGroup}>
+        <label htmlFor="">Author</label>
+        <input
+          value={author}
+          name="author"
+          onChange={handleChange}
+          className={styles.field}
+          type="text"
+          placeholder="Enter Author"
+        />
+      </div>
+      <button className={styles.btn}>Add book</button>
+    </form>
+  );
+};
+
+// class FormAddBook extends Component {
+//   state = {
+//     title: "",
+//     author: "",
+//   };
+
+//   handleChange = ({ target }) => {
+//     const { value, name } = target;
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     const { onSubmit } = this.props;
+//     onSubmit({ ...this.state });
+//     this.setState({
+//       title: "",
+//       author: "",
+//     });
+//   };
+
+//   render() {
+//     const { handleChange, handleSubmit } = this;
+//     const { title, author } = this.state;
+
+//     return (
+//       <form onSubmit={handleSubmit}>
+//         <div className={styles.formGroup}>
+//           <label htmlFor="">Title</label>
+//           <input
+//             value={title}
+//             name="title"
+//             onChange={handleChange}
+//             className={styles.field}
+//             type="text"
+//             placeholder="Enter Title"
+//           />
+//         </div>
+//         <div className={styles.formGroup}>
+//           <label htmlFor="">Author</label>
+//           <input
+//             value={author}
+//             name="author"
+//             onChange={handleChange}
+//             className={styles.field}
+//             type="text"
+//             placeholder="Enter Author"
+//           />
+//         </div>
+//         <button className={styles.btn}>Add book</button>
+//       </form>
+//     );
+//   }
+// }
 
 export default FormAddBook;
 
