62f82b4e5553d38fbfa1de6fc246cfafc8553f1b
RomaSamkov <Romansamkov.rs@gmail.com>
2023-01-15 23:36:46 +0200

    Update PostsSearchForm.jsx

--- src/components/PostSearch/PostsSearchForm/PostsSearchForm.jsx
+++ src/components/PostSearch/PostsSearchForm/PostsSearchForm.jsx
@@ -1,47 +1,77 @@
-import { Component } from "react";
+import { useState } from "react";
 import TextField from "../../../servises/components/TextField/TextField";
 import fields from "./fields";
 import initialState from "./initialState";
 import PropTypes from "prop-types";
-import { nanoid } from "nanoid";
+// import { nanoid } from "nanoid";
 import SubmitButton from "../../../servises/components/SubmitButton/SubmitButton";
 import styles from "./PostSearchForm.module.scss";
 
-class PostsSearchForm extends Component {
-  state = { ...initialState };
-  searchId = nanoid();
+const PostsSearchForm = ({ onSubmit }) => {
+  const [state, setState] = useState({ ...initialState });
 
-  handleChange = ({ target }) => {
+  const handleChange = ({ target }) => {
     const { value, name } = target;
-    this.setState({ [name]: value });
+    setState((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    const { onSubmit } = this.props;
-    onSubmit({ ...this.state });
-    this.setState({
-      title: "",
-      author: "",
-      search: "",
-    });
+    onSubmit({ ...state });
+    setState({ ...initialState });
   };
-  render() {
-    const { search } = this.state;
-    const { handleChange, handleSubmit, searchId } = this;
-    return (
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <TextField
-          value={search}
-          id={searchId}
-          handleChange={handleChange}
-          {...fields.search}
-        />
-        <SubmitButton text="Search" />
-      </form>
-    );
-  }
-}
+
+  const { search } = state;
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <TextField
+        value={search}
+        handleChange={handleChange}
+        {...fields.search}
+      />
+      <SubmitButton text="Search" />
+    </form>
+  );
+};
+// class PostsSearchForm extends Component {
+//   state = { ...initialState };
+//   searchId = nanoid();
+
+//   handleChange = ({ target }) => {
+//     const { value, name } = target;
+//     this.setState({ [name]: value });
+//   };
+
+//   handleSubmit = (e) => {
+//     e.preventDefault();
+//     const { onSubmit } = this.props;
+//     onSubmit({ ...this.state });
+//     this.setState({
+//       title: "",
+//       author: "",
+//       search: "",
+//     });
+//   };
+//   render() {
+//     const { search } = this.state;
+//     const { handleChange, handleSubmit, searchId } = this;
+//     return (
+//       <form onSubmit={handleSubmit} className={styles.form}>
+//         <TextField
+//           value={search}
+//           id={searchId}
+//           handleChange={handleChange}
+//           {...fields.search}
+//         />
+//         <SubmitButton text="Search" />
+//       </form>
+//     );
+//   }
+// }
 
 export default PostsSearchForm;
 
