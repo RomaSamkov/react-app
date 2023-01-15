a2754ca227436fa3676cdb55220fe1fd009f4fb6
RomaSamkov <Romansamkov.rs@gmail.com>
2023-01-15 23:36:48 +0200

    Update Modal.jsx

--- src/servises/components/Modal/Modal.jsx
+++ src/servises/components/Modal/Modal.jsx
@@ -1,35 +1,60 @@
-import { Component } from "react";
+import { useEffect } from "react";
 import styles from "./Modal.module.scss";
 
-class Modal extends Component {
-  componentDidMount() {
-    document.addEventListener("keydown", this.closeModal);
-  }
+const Modal = ({ close, children }) => {
+  useEffect(() => {
+    document.addEventListener("keydown", closeModal);
 
-  closeModal = ({ target, currentTarget, code }) => {
+    return () => document.removeEventListener("keydown", closeModal);
+  }, []);
+
+  const closeModal = ({ target, currentTarget, code }) => {
     if (target === currentTarget || code === "Escape") {
-      this.props.close();
+      close();
     }
   };
 
-  componentWillUnmount() {
-    document.removeEventListener("keydown", this.closeModal);
-  }
-
-  render() {
-    const { children } = this.props;
-    const { closeModal } = this;
-    return (
-      <div className={styles.overlay} onClick={closeModal}>
-        <div className={styles.modal}>
-          <span className={styles.close} onClick={closeModal}>
-            X
-          </span>
-          {children}
-        </div>
+  return (
+    <div className={styles.overlay} onClick={closeModal}>
+      <div className={styles.modal}>
+        <span onClick={closeModal} className={styles.close}>
+          X
+        </span>
+        {children}
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+// class Modal extends Component {
+//   componentDidMount() {
+//     document.addEventListener("keydown", this.closeModal);
+//   }
+
+//   closeModal = ({ target, currentTarget, code }) => {
+//     if (target === currentTarget || code === "Escape") {
+//       this.props.close();
+//     }
+//   };
+
+//   componentWillUnmount() {
+//     document.removeEventListener("keydown", this.closeModal);
+//   }
+
+//   render() {
+//     const { children } = this.props;
+//     const { closeModal } = this;
+//     return (
+//       <div className={styles.overlay} onClick={closeModal}>
+//         <div className={styles.modal}>
+//           <span className={styles.close} onClick={closeModal}>
+//             X
+//           </span>
+//           {children}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default Modal;
