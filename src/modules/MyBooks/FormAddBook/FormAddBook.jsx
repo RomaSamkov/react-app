7c33319a9caf87a6e80bea7a5d27958d18dd6bbe
RomaSamkov <Romansamkov.rs@gmail.com>
2023-01-16 16:29:48 +0200

    add src folder

--- src/modules/MyBooks/FormAddBook/FormAddBook.jsx
+++ src/modules/MyBooks/FormAddBook/FormAddBook.jsx
@@ -1,32 +1,52 @@
-import { useState } from "react";
+// import { useState } from "react";
 
-import useForm from "../../../shared/hooks/useForm"
+import useForm from "../../../shared/hooks/useForm";
 
 import styles from "./form-add-book.module.css";
 
 import { initialState } from "./initialState";
 
-const FormAddBook = ({onSubmit}) => {
-    const {state, handleChange, handleSubmit} = useForm({initialState, onSubmit})
+const FormAddBook = ({ onSubmit }) => {
+  const { state, handleChange, handleSubmit } = useForm({
+    initialState,
+    onSubmit,
+  });
 
-    const {title, author} = state;
+  const { title, author } = state;
 
-    return (
-        <form action="" onSubmit={handleSubmit}>
-            <div className={styles.group}>
-                <label className={styles.label} htmlFor="">Название:</label>
-                <input value={title} name="title" onChange={handleChange} className={styles.input} type="text" placeholder="Название книги" />
-            </div>
-            <div className={styles.group}>
-                <label className={styles.label} htmlFor="">Автор:</label>
-                <input value={author} name="author" onChange={handleChange} className={styles.input} type="text" placeholder="Автор книги" />
-            </div>
-            <div className={styles.group}>
-                <button type="submit">Добавить</button>
-            </div>
-        </form>
-    )
-
-}
+  return (
+    <form action="" onSubmit={handleSubmit}>
+      <div className={styles.group}>
+        <label className={styles.label} htmlFor="">
+          Название:
+        </label>
+        <input
+          value={title}
+          name="title"
+          onChange={handleChange}
+          className={styles.input}
+          type="text"
+          placeholder="Название книги"
+        />
+      </div>
+      <div className={styles.group}>
+        <label className={styles.label} htmlFor="">
+          Автор:
+        </label>
+        <input
+          value={author}
+          name="author"
+          onChange={handleChange}
+          className={styles.input}
+          type="text"
+          placeholder="Автор книги"
+        />
+      </div>
+      <div className={styles.group}>
+        <button type="submit">Добавить</button>
+      </div>
+    </form>
+  );
+};
 
 export default FormAddBook;
