40a205caca7d0d76b71c57909243dbcc3e3f6f8b
RomaSamkov <Romansamkov.rs@gmail.com>
2023-01-25 23:16:15 +0200

    Update posts.js

--- src/shared/services/posts.js
+++ src/shared/services/posts.js
@@ -1,27 +1,28 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://jsonplaceholder.typicode.com/posts",
-    params: {
-        _limit: 9
-    }
-})
+  baseURL: "https://jsonplaceholder.typicode.com/posts",
+  params: {
+    _limit: 12,
+  },
+});
 
 export const getPosts = async (_page = 1) => {
-    const {data} = await instance("/", {
-        params: {
-            _page,
-        }
-    });
-    return data;
-}
+  const { data } = await instance.get("/", {
+    params: {
+      _page,
+    },
+  });
+
+  return data;
+};
 
 export const searchPosts = async (q, _page = 1) => {
-    const {data} = await instance("/", {
-        params: {
-            q,
-            _page
-        }
-    });
-    return data;
-}
+  const { data } = await instance("/", {
+    params: {
+      q,
+      _page,
+    },
+  });
+  return data;
+};
