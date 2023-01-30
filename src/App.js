52f60c8d8eb87a2143d75a7abfeb1b8e9029631e
RomaSamkov <Romansamkov.rs@gmail.com>
2023-01-30 21:46:20 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
-// import ToggleButton from "./shared/components/ToggleButton";
-// import HeaderMenu from "./modules/HeaderMenu";
-// import MyBooks from "./modules/MyBooks";
-// import Tabs from "./modules/Tabs";
-
 import "./App.css";
 import Menu from "modules/Menu";
 import UserRoutes from "UserRoutes";
-
-// import menuItems from "./data/menu.json";
-// import tabsItems from "./data/tabs.json"
 
 function App() {
   return (
     <div className="App">
       <Menu />
       <UserRoutes />
-      {/* <Tabs items={tabsItems} /> */}
-      {/* <ToggleButton text="Click me" /> */}
-      {/* <HeaderMenu items={menuItems} /> */}
-      {/* <MyBooks /> */}
     </div>
   );
 }
