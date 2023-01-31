601661dc3bbdc974fe0faa9e3610a29ce7b03c79
RomaSamkov <Romansamkov.rs@gmail.com>
2023-01-31 19:05:41 +0200

    Update UserRoutes.jsx

--- src/UserRoutes.jsx
+++ src/UserRoutes.jsx
@@ -2,7 +2,7 @@ import BooksPage from "pages/BooksPage/BooksPage";
 import ContactsPage from "pages/ContactsPage/ContactsPage";
 import HomePage from "pages/HomePage/HomePage";
 import NotFoundPage from "pages/NotFoundPage/NotFoundPage";
-import PostsPage from "pages/PostsPage/PostsPage";
+import PostsSearchPage from "pages/PostsSearchPage/PostsSearchPage";
 import SinglePostsPage from "pages/SinglePostsPage/SinglePostsPage";
 import VotePage from "pages/VotePage/VotePage";
 import React from "react";
@@ -12,7 +12,7 @@ const UserRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
-      <Route path="/posts" element={<PostsPage />} />
+      <Route path="/posts" element={<PostsSearchPage />} />
       <Route path="/posts/:id" element={<SinglePostsPage />} />
       <Route path="/contacts" element={<ContactsPage />} />
       <Route path="/books" element={<BooksPage />} />
