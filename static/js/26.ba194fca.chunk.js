ff90c5e1426e1c8b481a32cc2df33e2baf40a818
RomaSamkov <92638707+RomaSamkov@users.noreply.github.com>
2023-02-01 20:16:21 +0000

    Deploying to gh-pages from @ RomaSamkov/react-app@91319d8987bf48f011131924c14f804ba8546cd5 🚀

---
+++ static/js/26.ba194fca.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_app=self.webpackChunkreact_app||[]).push([[26],{26:function(e,a,c){c.r(a);c(791);var n=c(184);a.default=function(){return(0,n.jsx)("div",{className:"container",children:(0,n.jsx)("h2",{children:"Contacts Page"})})}}}]);
+//# sourceMappingURL=26.ba194fca.chunk.js.map
---
+++ static/js/794.2e841975.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_app=self.webpackChunkreact_app||[]).push([[794],{794:function(e,a,n){n.r(a);n(791);var c=n(87),r=n(184);a.default=function(){return(0,r.jsxs)("div",{className:"container",children:[(0,r.jsx)("h2",{children:"Not found Page"}),(0,r.jsx)(c.rU,{to:"/",children:"To Main Page"})]})}}}]);
+//# sourceMappingURL=794.2e841975.chunk.js.map
