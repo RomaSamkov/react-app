74f0b8a6ff671dd4ea4af0564b06841d66bdf2a8
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-02 20:07:40 +0200

    Create store.js

---
+++ src/redux/store.js
@@ -0,0 +1,28 @@
+import { createStore } from "redux";
+
+const initialStore = {
+  books: [
+    {
+      id: "1",
+      title: "JS",
+      author: "Developer",
+      favorite: true,
+    },
+    {
+      id: "2",
+      title: "TS",
+      author: "Junior",
+      favorite: false,
+    },
+  ],
+};
+
+const reducer = (store = initialStore) => {
+  return store;
+};
+
+const store = createStore(reducer);
+
+export default store;
+
+console.log(store.getState());
