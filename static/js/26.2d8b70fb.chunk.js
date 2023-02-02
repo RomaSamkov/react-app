cea18a5841896224774890922a1ec57820a52301
RomaSamkov <92638707+RomaSamkov@users.noreply.github.com>
2023-02-02 18:08:52 +0000

    Deploying to gh-pages from @ RomaSamkov/react-app@3dfaa7d67ce5a69f587aec30c58ff85ef3817a04 🚀

---
+++ static/js/26.2d8b70fb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_app=self.webpackChunkreact_app||[]).push([[26],{2026:function(e,a,c){c.r(a);c(2791);var n=c(184);a.default=function(){return(0,n.jsx)("div",{className:"container",children:(0,n.jsx)("h2",{children:"Contacts Page"})})}}}]);
+//# sourceMappingURL=26.2d8b70fb.chunk.js.map
