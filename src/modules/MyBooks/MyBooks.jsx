c936e9e3c865a61763b2f4bab216bac43246d591
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-02 20:07:35 +0200

    add useSelector

--- src/modules/MyBooks/MyBooks.jsx
+++ src/modules/MyBooks/MyBooks.jsx
@@ -1,58 +1,60 @@
-import { useState } from "react";
-import { nanoid } from "nanoid";
+// import { useState } from "react";
+// import { nanoid } from "nanoid";
 
 import Container from "./Container";
 import FormAddBook from "./FormAddBook";
 import BookList from "./BookList";
 
 import styles from "./my-books.module.css";
+import { useSelector } from "react-redux";
 
 const MyBooks = () => {
-    const [books, setBooks] = useState([]);
-    const [filter, setFilter] = useState("");
+  const books = useSelector((store) => store.books);
+  // const [books, setBooks] = useState([]);
+  // const [filter, setFilter] = useState("");
 
-    const addBook = ({title, author}) => {
-        const newBook = {
-            title,
-            author,
-            id: nanoid()
-        };
-        setBooks(prevBooks => [...prevBooks, newBook]);
-    }
+  // const addBook = ({ title, author, favorite }) => {
+  //   const newBook = {
+  //     title,
+  //     author,
+  //     favorite,
+  //     id: nanoid(),
+  //   };
+  //   setBooks((prevBooks) => [...prevBooks, newBook]);
+  // };
 
-    const removeBook = (id) => {
-        setBooks(prevBooks => prevBooks.filter(item => item.id !== id))
-    }
+  // const removeBook = (id) => {
+  //   setBooks((prevBooks) => prevBooks.filter((item) => item.id !== id));
+  // };
 
-    const handleFilter = ({target}) => setFilter(target.value)
+  // const handleFilter = ({ target }) => setFilter(target.value);
 
-    const getFilteredBooks = () => {
-        if (!filter) {
-            return books;
-        }
-        const filterValue = filter.toLowerCase();
-        const filteredBooks = books.filter(({ title }) => {
-            const titleValue = title.toLowerCase();
-            return titleValue.includes(filterValue);
-        });
+  // const getFilteredBooks = () => {
+  //   if (!filter) {
+  //     return books;
+  //   }
+  //   const filterValue = filter.toLowerCase();
+  //   const filteredBooks = books.filter(({ title }) => {
+  //     const titleValue = title.toLowerCase();
+  //     return titleValue.includes(filterValue);
+  //   });
 
-        return filteredBooks;
-    }
+  //   return filteredBooks;
+  // };
 
-    const filteredBooks = getFilteredBooks();
+  // const filteredBooks = getFilteredBooks();
 
-    return (
-        <div className={styles.books}>
-            <Container title="Добавить книгу">
-                <FormAddBook onSubmit={addBook} />
-            </Container>
-            <Container title="Список книг">
-                <input onChange={handleFilter} name="filter" type="text" placeholder="Введите название " />
-                <BookList books={filteredBooks} removeBook={removeBook} />
-            </Container>
-        </div>
-    )
-
-}
+  return (
+    <div className={styles.books}>
+      <Container title="Add book">
+        <FormAddBook />
+      </Container>
+      <Container title="List of books">
+        <input name="filter" type="text" placeholder="Enter name of book" />
+        <BookList books={books} />
+      </Container>
+    </div>
+  );
+};
 
 export default MyBooks;
