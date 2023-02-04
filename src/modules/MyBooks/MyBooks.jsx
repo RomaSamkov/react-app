a8184261185005027b139ad2342d82f10f56bb54
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-04 22:09:24 +0200

    add onAddBook and onRemoveBook

--- src/modules/MyBooks/MyBooks.jsx
+++ src/modules/MyBooks/MyBooks.jsx
@@ -1,57 +1,32 @@
-// import { useState } from "react";
-// import { nanoid } from "nanoid";
-
 import Container from "./Container";
 import FormAddBook from "./FormAddBook";
 import BookList from "./BookList";
 
 import styles from "./my-books.module.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { addBook, removeBook } from "redux/actions";
+import { getBooks } from "redux/selectors";
 
 const MyBooks = () => {
-  const books = useSelector((store) => store.books);
-  // const [books, setBooks] = useState([]);
-  // const [filter, setFilter] = useState("");
+  const books = useSelector(getBooks);
+  const dispatch = useDispatch();
 
-  // const addBook = ({ title, author, favorite }) => {
-  //   const newBook = {
-  //     title,
-  //     author,
-  //     favorite,
-  //     id: nanoid(),
-  //   };
-  //   setBooks((prevBooks) => [...prevBooks, newBook]);
-  // };
-
-  // const removeBook = (id) => {
-  //   setBooks((prevBooks) => prevBooks.filter((item) => item.id !== id));
-  // };
-
-  // const handleFilter = ({ target }) => setFilter(target.value);
-
-  // const getFilteredBooks = () => {
-  //   if (!filter) {
-  //     return books;
-  //   }
-  //   const filterValue = filter.toLowerCase();
-  //   const filteredBooks = books.filter(({ title }) => {
-  //     const titleValue = title.toLowerCase();
-  //     return titleValue.includes(filterValue);
-  //   });
-
-  //   return filteredBooks;
-  // };
-
-  // const filteredBooks = getFilteredBooks();
+  const onAddBook = (payload) => {
+    const action = addBook(payload);
+    dispatch(action);
+  };
+  const onRemoveBook = (payload) => {
+    dispatch(removeBook(payload));
+  };
 
   return (
     <div className={styles.books}>
       <Container title="Add book">
-        <FormAddBook />
+        <FormAddBook onSubmit={onAddBook} />
       </Container>
       <Container title="List of books">
         <input name="filter" type="text" placeholder="Enter name of book" />
-        <BookList books={books} />
+        <BookList books={books} removeBook={onRemoveBook} />
       </Container>
     </div>
   );
