0f3b8aa12606251db6bfa8933b5b0a2ee96cd199
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-04 22:10:29 +0200

    add actions to reducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,26 +1,26 @@
 import { createStore } from "redux";
+import { ADD_BOOK, REMOVE_BOOK } from "./types";
 
 const initialStore = {
-  books: [
-    {
-      id: "1",
-      title: "JS",
-      author: "Developer",
-      favorite: true,
-    },
-    {
-      id: "2",
-      title: "TS",
-      author: "Junior",
-      favorite: false,
-    },
-  ],
+  books: [],
 };
 
-const reducer = (store = initialStore) => {
-  return store;
+const reducer = (store = initialStore, { type, payload }) => {
+  switch (type) {
+    case ADD_BOOK:
+      return { ...store, books: [...store.books, payload] };
+    case REMOVE_BOOK:
+      const newBooks = store.books.filter(({ id }) => id !== payload);
+      return { ...store, books: newBooks };
+
+    default:
+      return store;
+  }
 };
 
-const store = createStore(reducer);
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 export default store;
