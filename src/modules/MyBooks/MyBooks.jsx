ffc13fefc11ba889445865cb2101d0b75ce0d052
RomaSamkov <Romansamkov.rs@gmail.com>
2023-02-05 21:30:12 +0200

    add filter

--- src/modules/MyBooks/MyBooks.jsx
+++ src/modules/MyBooks/MyBooks.jsx
@@ -5,16 +5,19 @@ import BookList from "./BookList";
 import styles from "./my-books.module.css";
 import { useDispatch, useSelector } from "react-redux";
 import { addBook, removeBook } from "redux/actions";
-import { getBooks } from "redux/selectors";
+import { getBooks, getFilter } from "redux/selectors";
 
 const MyBooks = () => {
   const books = useSelector(getBooks);
+  const filter = useSelector(getFilter);
+
   const dispatch = useDispatch();
 
   const onAddBook = (payload) => {
     const action = addBook(payload);
     dispatch(action);
   };
+
   const onRemoveBook = (payload) => {
     dispatch(removeBook(payload));
   };
@@ -25,7 +28,12 @@ const MyBooks = () => {
         <FormAddBook onSubmit={onAddBook} />
       </Container>
       <Container title="List of books">
-        <input name="filter" type="text" placeholder="Enter name of book" />
+        <input
+          value={filter}
+          name="filter"
+          type="text"
+          placeholder="Enter name of book"
+        />
         <BookList books={books} removeBook={onRemoveBook} />
       </Container>
     </div>
