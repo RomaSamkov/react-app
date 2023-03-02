13bb2f2bc096da201d588c7a0254e3bcb668b38e
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-02 20:57:44 +0200

    Update HomePage.jsx

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,11 @@
 import Posts from "modules/Posts/Posts";
 import React from "react";
+import styles from "./HomePage.module.scss";
 
 const HomePage = () => {
   return (
     <div className="container">
-      <h2>Home Page</h2>
+      <h2 className={styles.title}>Posts :</h2>
       <Posts />
     </div>
   );
