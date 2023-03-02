845e0962bb7b880d70fc08cb002a933bc14dd19a
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-02 20:57:40 +0200

    Update PostList.jsx

--- src/modules/PostList/PostList.jsx
+++ src/modules/PostList/PostList.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import styles from "./PostList.module.scss";
 
 const PostList = ({ items }) => {
   const location = useLocation();
 
   const elements = items.map(({ id, title }) => (
-    <li key={id}>
-      <Link state={{ from: location }} to={`/posts/${id}`}>
+    <li key={id} className={styles.item}>
+      <Link
+        state={{ from: location }}
+        to={`/posts/${id}`}
+        className={styles.link}
+      >
         {title}
       </Link>
     </li>
