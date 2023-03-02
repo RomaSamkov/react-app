37603b3cc4b3e6fcc9c667c4e013113a49e65690
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-02 20:58:01 +0200

    Update MyFavoriteBooksPage.jsx

--- src/pages/MyFavoriteBooksPage/MyFavoriteBooksPage.jsx
+++ src/pages/MyFavoriteBooksPage/MyFavoriteBooksPage.jsx
@@ -1,14 +1,42 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { removeBook } from "redux/actions";
 import { getFavoriteBooks } from "redux/selectors";
+import styles from "./MyFavoriteBooksPage.module.scss";
 
 const MyFavoriteBooksPage = () => {
   const books = useSelector(getFavoriteBooks);
-  const elements = books.map(({ id, title }) => <li key={id}>{title}</li>);
+
+  const dispatch = useDispatch();
+
+  const onRemoveBook = (payload) => {
+    dispatch(removeBook(payload));
+  };
+
+  const elements = books.map(({ id, title, author }) => (
+    <li key={id} className={styles.item}>
+      <div className={styles.wrapper}>
+        <p className={styles.text}>
+          Title :<span className={styles.itemText}> "{title}".</span>
+        </p>{" "}
+        <p>
+          Author: <span className={styles.itemText}> {author}.</span>
+        </p>
+        <button
+          type="button"
+          className={styles.btn}
+          onClick={() => onRemoveBook(id)}
+        >
+          X
+        </button>
+      </div>
+    </li>
+  ));
+
   return (
-    <div className="container">
-      <h2>My Favorite Books</h2>
-      <ul>{elements}</ul>
+    <div className={styles.container}>
+      <h2 className={styles.title}>My Favorite Books</h2>
+      <ol className={styles.list}>{elements}</ol>
     </div>
   );
 };
