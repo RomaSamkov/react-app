3383be165056d39aa040a13d4f04c31b6205f934
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-02 20:58:34 +0200

    Update NotFoundPage.jsx

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./NotFoundPage.module.scss";
 
 const NotFoundPage = () => {
   return (
-    <div className="container">
-      <h2>Not found Page</h2>
-      <Link to="/">To Main Page</Link>
+    <div className={styles.container}>
+      <h2 className={styles.fontFaceText}>
+        This is my student draft while studying :{" "}
+      </h2>
+      <ul>
+        <li className={styles.fontFaceText}>Redux</li>
+        <li className={styles.fontFaceText}>HTTP requests in Redux </li>
+        <li className={styles.fontFaceText}>used JSONPlaceholder</li>
+      </ul>
+      <Link className={styles.fontLink} to="/">
+        To Main Page
+      </Link>
     </div>
   );
 };
