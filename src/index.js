f38cc33ea09be82eca5afa0692dc12e32c306668
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-02 20:57:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,21 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import store from "redux/store";
 import { Provider } from "react-redux";
+import "./fonts/BoyzRGrossShadowNF.ttf";
+import "./fonts/ChanticleerRomanNF-Bold.ttf";
+import "./fonts/LuckiestGuy.ttf";
 
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <BrowserRouter basename="/react-app/">
+      <App />
+    </BrowserRouter>
+  </Provider>
+  // </React.StrictMode>
 );
+
+// basename = "/react-app/";
