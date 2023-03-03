033145e8bb39a65cf2dd2e74aa64cfa38832691e
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-03 20:35:48 +0200

    Update VoteResults.jsx

--- src/modules/Vote/VoteResults.jsx
+++ src/modules/Vote/VoteResults.jsx
@@ -3,21 +3,32 @@ import styles from "./Vote.module.scss";
 const VoteResults = ({ total, JS, Phyton, jsPercentage, phytonPercentage }) => {
   return (
     <>
-      <div className={styles.btnBlock}>
-        For JS:
-        <ul>
-          <li>All:{JS}</li>
-          <li>In percentage:{jsPercentage}%</li>
+      <div className={styles.result}>
+        <h2 className={styles.name}>For JavaScript:</h2>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            All : <span className={styles.span}>{JS}</span>
+          </li>
+          <li className={styles.item}>
+            In percentage :<span className={styles.span}>{jsPercentage}%</span>
+          </li>
         </ul>
       </div>
-      <div className={styles.btnBlock}>
-        For Phyton:
-        <ul>
-          <li>All:{Phyton}</li>
-          <li>In percentage:{phytonPercentage}%</li>
+      <div className={styles.result}>
+        <h2 className={styles.name}>For Phyton:</h2>
+        <ul className={styles.list}>
+          <li className={styles.item}>
+            All : <span className={styles.span}>{Phyton}</span>
+          </li>
+          <li className={styles.item}>
+            In percentage :
+            <span className={styles.span}>{phytonPercentage}%</span>
+          </li>
         </ul>
       </div>
-      <div className={styles.btnBlock}>All votes: {total} votes</div>
+      <div className={styles.resultBlock}>
+        All votes : <span className={styles.span}> {total}</span>
+      </div>
     </>
   );
 };
