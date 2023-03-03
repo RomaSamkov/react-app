dbbe8e445fcf7b6de2c4fb557fe7defd893959e6
RomaSamkov <Romansamkov.rs@gmail.com>
2023-03-03 20:35:42 +0200

    Update VoteActions.jsx

--- src/modules/Vote/VoteActions.jsx
+++ src/modules/Vote/VoteActions.jsx
@@ -2,14 +2,15 @@ import styles from "./Vote.module.scss";
 
 const VoteActions = ({ handleVote }) => {
   return (
-    <>
-      <div className={styles.btnBlock}>
-        <button onClick={() => handleVote("JS")}>JS</button>
-      </div>
-      <div className={styles.btnBlock}>
-        <button onClick={() => handleVote("Phyton")}>Phyton</button>
-      </div>
-    </>
+    <div className={styles.btnBlock}>
+      <button className={styles.btn} onClick={() => handleVote("JS")}>
+        JavaScript
+      </button>
+
+      <button className={styles.btn} onClick={() => handleVote("Phyton")}>
+        Phyton
+      </button>
+    </div>
   );
 };
 
